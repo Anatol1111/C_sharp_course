ef5acbcc6fe1306585ff6dd967d5316837b481d3
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-06-26 22:03:52 +0600

    homework_3_3

---
+++ 1_Lesson/homework_3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.Write("Вы ввели четное число = ");
+    Console.WriteLine(num);
+}
+else
+{
+    Console.Write("Вы ввели нечетное число");
+}
